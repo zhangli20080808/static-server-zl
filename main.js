0406a56a7baf97d3f891bbff1a18ff402aaa3b7c
zhangli20080808 <791492434@qq.com>
2021-09-30 14:32:57 +0800

    读取文件

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let http = require('http');
-let url = require('url');
 let path = require('path');
+let url = require('url');
 let fs = require('fs').promises;
-let { createReadStream } = require('fs');
+let { createReadStream } = require('fs'); // 可读流，可写流
 let mime = require('mime');
 
 const merge = (config) => {
@@ -17,15 +17,42 @@ const merge = (config) => {
 class Server {
   constructor(config) {
     this.config = merge(config);
-    console.log(this.config);
   }
-  handleRequest(req, res) {
+  async handleRequest(req, res) {
     console.log(this);
+    // 判断是文件夹还是文件
+    console.log(req.url);
+    let { pathname } = url.parse(req.url);
+    // const myUrl = new URL(req.url);
+    pathname = decodeURIComponent(pathname); // 对路径进行解码
+    let absPath = path.join(this.config.directory, pathname);
+    console.log(absPath, 'absPath');
+    try {
+      let statObj = await fs.stat(absPath);
+      if (statObj.isFile()) {
+        // 如果是文件，需要读取文件中的内容
+        this.sendFile(req, res, absPath);
+      }
+    } catch (e) {
+      this.sendError(req, res, e);
+    }
+  }
+  //
+  sendFile(req, res, absPath) {
+    // 采用流的方式 pipe  原理就是 监听 on data事件 ，拿到数据之后会调用 res.write,end方法把结果抛出去
+    res.setHeader('Content-Type', mime.getType(absPath), 'charset=utf-8');
+    // 注意 异步读取
+    createReadStream(absPath).pipe(res);
+  }
+  sendError(req, res, e) {
+    console.log(e);
+    res.statusCode = 404;
+    res.end('Not Found');
   }
   start() {
     // 如果不进行bind，this默认是http.createServer
     let server = http.createServer(this.handleRequest.bind(this));
-    server.listen(this.config.port,()=>{
+    server.listen(this.config.port, () => {
       console.log(`服务启动在${this.config.port}端口`);
     });
   }
